e49f9b06f5a26038fed9ac69fc4f1eace911754c
TareqJudehGithub <tareq.joudeh@gmail.com>
2020-06-17 16:57:41 +0300

    Installed and configured Mongoose, and with all needed connections and setup between Express server and Mongo

---
+++ config/db.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async() => {
+     try {
+          await mongoose.connect(db, {
+               useNewUrlParser: true,
+               useCreateIndex: true,
+               useFindAndModify: false,
+               useUnifiedTopology: true
+          });
+
+          console.log("MongoDB is connected!");
+     } catch (error) {
+          console.log(error.message);
+          process.exit(1);
+     }
+};
+module.exports = connectDB;
+
+
+
+//Promises:
+
+// const connectDB = () => {
+//      mongoose.connect(db, {
+//           useNewUrlParser: true,
+//           useCreateIndex: true,
+//           useFindAndModify: false,
+//           useUnifiedTopology: true
+//      })
+//      .then(() => console.log("MongoDB is connected!"))
+//      .catch(err => {
+//           console.log(err.message);
+//           process.exit(1);
+//      });
+// };
