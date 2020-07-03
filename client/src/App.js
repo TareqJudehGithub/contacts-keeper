cb9a075a5ee761a289c8de52c21e6e246ec199f9
TareqJudehGithub <tareq.joudeh@gmail.com>
2020-07-03 16:16:36 +0300

    Implemented Alert Context state management and component, passing it to Register component for form validations.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import ContactState from "./context/contact/ContanctState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 
 import './App.css';
@@ -16,22 +18,25 @@ import './App.css';
 const App = () => {
   return (
     <AuthState>
-       <ContactState>
-      <React.Fragment>
-        <Navbar 
-          title="Contact Keeper"
-          icon="fas fa-id-card-alt"
-        />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login}/>
-          </Switch>
-        </div>
-      </React.Fragment>
-    </ContactState>
+      <ContactState>
+        <AlertState>
+          <React.Fragment>
+            <Navbar 
+              title="Contact Keeper"
+              icon="fas fa-id-card-alt"
+            />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login}/>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </AlertState>
+      </ContactState>
     </AuthState>
    
   );
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import AlertContext from "../../context/alert/alertContext";
+
 
 const Register = () => {
 
+     const alertContext = useContext(AlertContext);
+     const { setAlert } = alertContext;
      const [user, setUser] = useState({
           name: "",
           email: "",
@@ -20,8 +24,23 @@ const Register = () => {
      };
      const onsubmitHandler = (event) => {
           event.preventDefault();
-          console.log("Register Submit");
-     }
+
+          if(name === "") {
+               setAlert("Please enter a valid Name.", "danger");
+          }
+          else if(email === ""){
+               setAlert("Please enter a valid email address.", "danger")
+          }
+          else if(password === ""){
+               setAlert("Password must not be empty!","danger")
+          }
+          else if( password2 !== password) {
+               setAlert("Password and Confirm Password must match.", "danger");
+          }
+          else{
+               console.log("Register Submit");
+          }
+     };
      
      return (
           <div className="form-container">
@@ -32,29 +51,35 @@ const Register = () => {
                     <div className="form-group">
                          <label htmlFor="name">Name</label>
                          <input type="text" name="name" value={name}
-                              onChange={onChangeHandler}
+                              placeholder="John Smith" autoComplete="name"
+                              onChange={onChangeHandler} required
                          />
                     </div>
 
                     <div className="form-group">
                          <label htmlFor="email" name="email">Email</label>
                          <input type="email" name="email" value={email}
-                              onChange={onChangeHandler}
+                              placeholder="example@email.com" autoComplete="email"
+                              onChange={onChangeHandler} required
                          />
                     </div>
 
                      
                     <div className="form-group">
                          <label htmlFor="password" name="password">Password</label>
-                         <input type="password" name="password" value={password}
+                         <input 
+                              type="password" name="password" value={password}
+                              placeholder="Enter password" autoComplete="password"
+                              minLength="6" maxLength="12" required 
                               onChange={onChangeHandler}
                          />
                     </div>
 
                     
                     <div className="form-group">
-                         <label htmlFor="password2" name="password2">Password</label>
+                         <label htmlFor="password2" name="password2">Confirm Password</label>
                          <input type="password" name="password2" value={password2}
+                         placeholder="Confirm your password" autoComplete="password2"
                               onChange={onChangeHandler}
                          />
                     </div>
