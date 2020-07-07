2550fe30b76cbafff681a866462a711b17c985fe
TareqJudehGithub <tareq.joudeh@gmail.com>
2020-07-08 02:13:06 +0300

    Preparing project for Heroku deployment.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -9,10 +10,14 @@ connectDB();
 //init Middleware
 app.use(express.json())
 
-
-app.get("/", (req, res) => {
-     res.json({ msg: "<h1>Hello Express!</h2>"});
-})
+// Serve static assets in production;
+if(process.env.NODE_ENV === "production"){
+     // Set static folder:
+     app.use(express.static("client/build"));
+};
+// app.get("*", (req, res) => 
+//      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+// );
 
 //routes
 app.use("/api/users", require("./routes/users"));
