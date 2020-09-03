f861f78eff3f306a4fb1a471de9fedf1b5e4a680
TareqJudehGithub <tareq.judeh@outlook.com>
2020-09-04 00:34:34 +0200

    Updated all packages. Fixing all security issues.

--- config/db.js
+++ config/db.js
@@ -2,27 +2,25 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-const connectDB = async() => {
-     try {
-          await mongoose.connect(db, {
-               useNewUrlParser: true,
-               useCreateIndex: true,
-               useFindAndModify: false,
-               useUnifiedTopology: true
-          });
-
-     } 
-     catch (error) {
-          console.log(error.message);
-          process.exit(1);
-     }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
 //Mongoose DB connection test:
-var Mongodb = mongoose.connection;
-Mongodb.on('error', console.error.bind(console, 'connection error:'));
-Mongodb.once('open', function() {
-  console.log("We're connected to Mongoose!")
+const Mongodb = mongoose.connection;
+Mongodb.on("error", console.error.bind(console, "connection error:"));
+Mongodb.once("open", function () {
+  console.log("We're connected to Mongoose!");
 });
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require("config");
 const connectDB = require("./config/db");
 const path = require("path");
 
@@ -8,14 +9,14 @@ const app = express();
 connectDB();
 
 //init Middleware
-app.use(express.json())
+app.use(express.json());
 
 // Serve static assets in production;
-if(process.env.NODE_ENV === "production"){
-     // Set static folder:
-     app.use(express.static("client/build"));
-};
-// app.get("*", (req, res) => 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder:
+  app.use(express.static("client/build"));
+}
+// app.get("*", (req, res) =>
 //      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
 // );
 
